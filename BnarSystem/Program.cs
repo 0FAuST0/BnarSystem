9a0229be08f78535dbd341873706a3dfd7bff9bd
0FAuST0 <bnbmon222@gmail.com>
2022-11-26 19:16:05 +0200

    not bufer

--- BnarSystem/Program.cs
+++ BnarSystem/Program.cs
@@ -9,7 +9,7 @@ for (; startNumber > 0;)
     processNumberInt = startNumber / 2;
     processNumber = startNumber % 2;
     string bufer = processNumber.ToString();
-    numberIs2 += bufer;
+    numberIs2 += bufe;
     startNumber = processNumberInt;
 
 }
