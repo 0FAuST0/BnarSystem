d9302f709819d1b62e38de9de01a9b8fbcc322ae
andro <andro@DESKTOP-3B3I4OA>
2022-11-26 18:34:43 +0200

    Add project files.

---
+++ BnarSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int startNumber = Convert.ToInt32(Console.ReadLine());
+int processNumber = 0;
+int processNumberInt = 0;
+string numberIs2 = "";
+for (; startNumber > 0;)
+{
+
+    processNumberInt = startNumber / 2;
+    processNumber = startNumber % 2;
+    string bufer = processNumber.ToString();
+    numberIs2 += bufer;
+    startNumber = processNumberInt;
+
+}
+char[] chars = numberIs2.ToCharArray();
+int i = numberIs2.Length-1;
+foreach (char coll in numberIs2)
+{
+    chars[i] = coll;
+     i--;   
+}
+
+Console.WriteLine(numberIs2);
+Console.WriteLine(chars);
+Console.Write(chars);
+
